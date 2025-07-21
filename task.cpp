92426dd456bc9470fecc2e3559a4c730821a9334
Mohd Kaif <mohdkaif200314@gmail.com>
2025-07-21 23:35:48 +0530

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,12 +1,16 @@
 #include "task.h"
 #include <iostream>
+using namespace std;
 
-Task::Task(int id, std::string title, std::string desc, int priority, std::string deadline)
+Task::Task(int id, string title, string desc, int priority, string deadline)
     : id(id), title(title), description(desc), priority(priority), deadline(deadline) {}
 
 void Task::display() const {
-    std::cout << "ID: " << id << "\nTitle: " << title
-              << "\nDescription: " << description
-              << "\nPriority: " << priority
-              << "\nDeadline: " << deadline << "\n";
+    cout << "\n------------------------------------------------------------\n";
+    cout << " ID       : " << id << "\n";
+    cout << " Title    : " << title << "\n";
+    cout << " Description : " << description << "\n";
+    cout << " Priority : " << (priority == 1 ? "High" : (priority == 2 ? "Medium" : "Low")) << "\n";
+    cout << " Deadline : " << deadline << "\n";
+    cout << "------------------------------------------------------------\n";
 }
