90716fba3784272caba835bebf8e9f66ac89deeb
Mohd Kaif <mohdkaif200314@gmail.com>
2025-07-21 23:36:09 +0530

    Update task.h

--- task.h
+++ task.h
@@ -8,11 +8,10 @@ public:
     int id;
     std::string title;
     std::string description;
-    int priority; // lower number = higher priority
-    std::string deadline; // e.g., "2025-05-15"
+    int priority;
+    std::string deadline;
 
     Task(int id, std::string title, std::string desc, int priority, std::string deadline);
-
     void display() const;
 };
 
